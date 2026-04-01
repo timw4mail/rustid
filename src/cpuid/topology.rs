207b7221caeb06717d5202f09366194e8ce6de4d
Timothy J. Warren <tim@timshome.page>
2026-04-01 09:42:18 -0400

    Remove build error

--- src/cpuid/topology.rs
+++ src/cpuid/topology.rs
@@ -1,6 +1,6 @@
 use super::brand::{VENDOR_AMD, VENDOR_INTEL};
 use super::{
-    EXT_LEAF_26, LEAF_0B, LEAF_1F, has_ht, is_amd, is_valid_leaf, logical_cores, vendor_str,
+    EXT_LEAF_26, LEAF_0B, LEAF_1F, is_amd, is_valid_leaf, logical_cores, vendor_str,
     x86_cpuid_count,
 };
 use crate::common::cache::Cache;
