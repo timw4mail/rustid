b7059868d8df971bb699eca05cc8e93e2edb6024
Timothy J. Warren <tim@timshome.page>
2026-04-07 10:09:21 -0400

    Exclude core_affinity crate from x86

--- src/common/cores.rs
+++ src/common/cores.rs
@@ -2,9 +2,13 @@
 
 /// Returns the number of logical cores (threads).
 pub fn logical_cores() -> usize {
+    #[cfg(not(target_arch = "x86"))]
     if let Some(cores) = core_affinity::get_core_ids() {
         cores.len()
     } else {
         1
     }
+
+    #[cfg(target_arch = "x86")]
+    1
 }
