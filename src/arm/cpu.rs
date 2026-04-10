24029586fbeb20d12e82ae739ed62947c0369eae
Timothy J Warren <tim@timshomepage.net>
2026-04-10 12:38:33 -0400

    Add some todos to arm detection, to be able to detect heterogenous cpu cores

--- src/arm/cpu.rs
+++ src/arm/cpu.rs
@@ -6,6 +6,7 @@ use super::*;
 use crate::common::*;
 use std::collections::BTreeMap;
 
+// TODO: Wrap raw_mdir and midr fields into HashSets or some other collection type
 #[derive(Debug, Default, PartialEq)]
 pub struct Cpu {
     pub raw_midr: usize,
@@ -16,6 +17,8 @@ pub struct Cpu {
 }
 
 impl TCpu for Cpu {
+    // TODO: Update detection logic to probe every thread so that Big.Little cpus
+    // have all their core types detected
     fn detect() -> Self {
         let cores: BTreeMap<CoreType, CpuCore> = BTreeMap::new();
         let raw_midr = super::get_midr();
