df2743dc347a2b9e237f12a7cce25911dbedd297
Timothy J Warren <tim@timshomepage.net>
2026-09-02 19:48:16 -0400

    Move windows gui implementation into windows sub module

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,11 +1,2 @@
-//! Top-level GUI module for rustid-gui.
-
-pub mod dialogs;
-pub mod menu;
-pub mod rtf;
-pub mod shims;
-pub mod state;
-pub mod theme;
-pub mod window;
-
-pub use window::run;
+#[cfg(target_os = "windows")]
+pub mod windows;
